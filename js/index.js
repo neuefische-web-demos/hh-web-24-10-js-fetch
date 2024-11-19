07335f8086d7e92d4af8b75213d78114d1e314eb
Peter Anderson <pete.anderson@gmail.com>
2024-11-19 11:16:29 +0100

    initial commit

---
+++ js/index.js
@@ -0,0 +1,36 @@
+// Import Functions & Modules
+import { JokeSection } from "../components/JokeSection/JokeSection.js";
+import { Joke } from "../components/Joke/Joke.js";
+
+// Declare root Element
+const root = document.body;
+
+// Assemble DOM for joke section
+const jokeSection = JokeSection();
+root.append(jokeSection);
+
+function renderJoke(joke) {
+  // Clear the joke section
+  jokeSection.innerHTML = "";
+
+  // Create a new joke DOM element and append it to joke section
+  const newJoke = Joke(joke);
+
+  jokeSection.append(newJoke);
+}
+
+async function getJoke() {
+  const response = await fetch(
+    "https://example-apis.vercel.app/api/bad-jokes/random"
+  );
+  const data = await response.json();
+
+  console.log("data: ", data);
+  console.log("joke?: ", typeof data.joke);
+  renderJoke(data.joke);
+}
+
+// renderJoke("How do you make a developer cheer?");
+console.log("A");
+getJoke();
+console.log("B");
---
+++ test.js
@@ -0,0 +1,9 @@
+console.log("hello world");
+const person = { name: "pete" };
+console.log("A");
+for (let i = 0; i <= 100000000; i++) {
+  //
+}
+console.log("B");
+
+// Meet back at 10:27
